694ec5621ba0e3bb92d768e432ba68eb364bcbda
Hernán Ezequiel Novara <her.novara@hotmail.com>
2023-12-16 12:59:48 -0300

    Ejercicios 1, 2, 3 y 4

---
+++ parte-1/js/index.js
@@ -0,0 +1,32 @@
+// Ejercicio 2:
+class Libro {
+    constructor (titulo, autor) {
+        this.autor = autor;
+        this.titulo = titulo;
+    }
+    descripcion() {
+        return (`Libro {autor: "${this.autor}", titulo: "${this.titulo}"}`)
+    }
+    // Ejercicio 3:
+    // let desc3y4 = (`"${this.titulo}, de ${this.autor.toUpperCase()}"`)
+    mostrarDatosEnConsola() {
+        // console.log(desc3y4)
+        console.log(`"${this.titulo}, de ${this.autor.toUpperCase()}"`)
+    }
+    // Ejercicio 4:
+    mostrarDatosEnAlert() {
+        // alert(desc3y4)
+        alert(`"${this.titulo}, de ${this.autor.toUpperCase()}"`)
+    }
+}
+let unLibro = new Libro('Ángeles y Demonios', 'Dan Brown')
+
+unLibro.descripcion()
+unLibro.mostrarDatosEnConsola() 
+unLibro.mostrarDatosEnAlert()
+
+console.log(unLibro)
+
+
+
+
